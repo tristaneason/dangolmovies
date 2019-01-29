edde2c19fc46052422f717b9405a6eb2d3829573
Tristan Eason <tristaneason@protonmail.com>
2019-01-29 03:33:24 -0500

    Create middleware array

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,25 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
+import ReduxThunk from 'redux-thunk'
 import rootReducer from '../reducers/rootReducer'
 
 const logger = store => next => action => {
-    console.log('action:', action);
-    return next(action);
+    console.log('action:', action)
+    return next(action)
 }
 
 export default function configureStore(state = {}) {
     const middleware = []
 
+    middleware.push(ReduxThunk)
+
     if (process.env.NODE_ENV === 'development') {
-        middleware.push(logger);
+        middleware.push(logger)
     }
 
     return createStore(
         rootReducer,
         compose(
-            applyMiddleware(thunk),
+            applyMiddleware(...middleware),
             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
         )
     )
