6d3899bde633c459e6155fdb6c1093268e72bd89
Tristan Eason <tristaneason@protonmail.com>
2019-01-29 12:46:07 -0500

    Commented out devtools arg

--- src/store/index.js
+++ src/store/index.js
@@ -20,7 +20,7 @@ export default function configureStore(state = {}) {
         rootReducer,
         compose(
             applyMiddleware(...middleware),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
         )
     )
 }
