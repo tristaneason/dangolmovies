8bef5270deee89358ce035e2d4862450b14165e1
Tristan Eason <tristaneason@protonmail.com>
2019-01-30 01:18:13 -0500

    Removed Thumbnail component

--- src/presentation/Favorites.js
+++ src/presentation/Favorites.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Thumbnail from '../components/Thumb';
 import { fetchMovies, addToFavorites, removeFromFavorites } from '../actions';
 import { favoritesSelector } from '../reducers';
 import './Favorites.css';
+import './Thumb.css';
 
 class Favorites extends Component  {
     componentDidMount() {
@@ -11,15 +11,20 @@ class Favorites extends Component  {
     }
 
     render() {
+        let heart;
+        if (this.props.favorited) heart = 'fas fa-heart';
+        else heart = 'far fa-heart';
+        
         return <section className="favorites container">
             <h2 className="text-center">Favorites</h2>
             <div className="row">
                 {this.props.favorites.map(item => {
                     const { favorite } = item;
-                    return <Thumbnail
-                        key={favorite.ids.trakt}
-                        name={favorite.name}
-                        year={favorite.year} />
+                    return <div key={favorite.ids.trakt} className="thumb col-sm-6 col-md-4 col-lg-3">
+                        <span className="thumb-name">{favorite.name}</span>
+                        <span className="thumb-year">{favorite.year}</span>
+                        <i className={heart}></i>
+                    </div>
                 })}
             </div>
         </section>
--- src/presentation/Home.js
+++ src/presentation/Home.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Thumbnail from '../components/Thumb';
 import { fetchMovies, addToFavorites, removeFromFavorites } from '../actions';
 import { favoritesSelector } from '../reducers';
 import './Home.css';
+import './Thumb.css';
 
 class Home extends Component {
     componentDidMount() {
@@ -11,15 +11,20 @@ class Home extends Component {
     }
 
     render() {
+        let heart;
+        if (this.props.favorited) heart = 'fas fa-heart';
+        else heart = 'far fa-heart';
+
         return <section className="home container">
             <h2 className="text-center">Top 20 Trending Movies</h2>
             <div className="row">
                 {this.props.movies.map(item => {
                     const { movie } = item;
-                    return <Thumbnail
-                        key={movie.ids.trakt}
-                        name={movie.title}
-                        year={movie.year} />
+                    return <div key={movie.ids.trakt} className="thumb col-sm-6 col-md-4 col-lg-3">
+                        <span className="thumb-name">{movie.name}</span>
+                        <span className="thumb-year">{movie.year}</span>
+                        <i className={heart}></i>
+                    </div>
                 })}
             </div>
         </section>
