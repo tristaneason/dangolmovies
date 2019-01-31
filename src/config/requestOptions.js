b048a7f9e854d36b6f5a5ccb51d1fb2f7fb8a763
Tristan Eason <tristaneason@protonmail.com>
2019-01-31 11:55:11 -0500

    Converted to use only one http client. Moved req opts to own file

---
+++ src/config/requestOptions.js
@@ -0,0 +1,9 @@
+export const requestOptions = {
+    method: 'GET',
+    url: 'https://api.trakt.tv/movies/trending?limit=20',
+    headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': 'a4503dd80104293a73f0e02c56e4c5bdd34340402c36cdde4492039c67e9af91'
+    }
+}
