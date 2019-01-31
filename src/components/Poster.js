440646ba77ee975e28691f43e7465b71a9f073a1
Tristan Eason <tristaneason@protonmail.com>
2019-01-30 21:12:50 -0500

    Working poster images

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-const Poster = props => {
-    let posterUrl;
-    axios.get(`https://api.themoviedb.org/3/movie/${props.id}?api_key=d1d57c1b46fefbe658f61409b5f533c9`)
-        .then(response => {
-            posterUrl = `https://image.tmdb.org/t/p/w500${response.data.poster_path}`;
-            console.log(posterUrl);
-            return posterUrl;
-        })
-        .catch(error => {
-            throw new Error(error);
-        })
-    return <img src={posterUrl} className="image" alt={props.alt} />
+class Poster extends Component {
+    state = {
+        poster: null
+    }
+
+    render() {
+        axios.get(`https://api.themoviedb.org/3/movie/${this.props.id}?api_key=d1d57c1b46fefbe658f61409b5f533c9`)
+            .then(response => {
+                const posterUrl = `https://image.tmdb.org/t/p/w500${response.data.poster_path}`;
+                this.setState({ poster: posterUrl });
+            })
+            .catch(error => {
+                throw new Error(error);
+            })
+        return <img src={this.state.poster} className="image" alt={this.props.alt} />
+    }
 }
 
 export default Poster;
