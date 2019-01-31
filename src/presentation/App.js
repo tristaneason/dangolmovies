ab210ab47665a5f91a55784e42ca781f92f7f84d
Tristan Eason <tristaneason@protonmail.com>
2019-01-30 22:50:54 -0500

    Added banner

--- src/presentation/App.js
+++ src/presentation/App.js
@@ -4,23 +4,27 @@ import { Provider } from 'react-redux';
 import Home from './Home';
 import Favorites from './Favorites';
 import './App.css';
+import banner from '../static/boomhauer.jpg';
 
 export default ({ reduxStore }) => (
     <Provider store={reduxStore}>
         <Router>
-            <main className="app container">
-                <h1 className="text-center">Dang ’ol Movies</h1>
-                <ul className="nav nav-tabs justify-content-center">
-                    <li>
-                        <Link className="nav-link" to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link className="nav-link" to="/favorites">Favorites</Link>
-                    </li>
-                </ul>
-                <Route path="/" exact component={Home} />
-                <Route path="/favorites" component={Favorites} />
-            </main>
+            <div>
+                <h1 class="text-center">Dang ’ol Movies</h1>
+                <img class="banner" src={banner} alt="banner" />
+                <main className="app container">
+                    <ul className="nav nav-tabs justify-content-center">
+                        <li>
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link className="nav-link" to="/favorites">Favorites</Link>
+                        </li>
+                    </ul>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/favorites" component={Favorites} />
+                </main>
+            </div>
         </Router>
     </Provider>
 )
